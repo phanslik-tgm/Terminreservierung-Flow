47654db9de3f0ba09d2e432fd4f61c847ee84f16
phanslik-tgm <peterhanslik1010@gmail.com>
2018-06-11 15:32:35 +0200

    event hinzugefügt

--- src/main/java/com/sew/backend/data/entity/Event.java
+++ src/main/java/com/sew/backend/data/entity/Event.java
@@ -24,10 +24,67 @@ import javax.validation.constraints.NotNull;
 import org.hibernate.annotations.BatchSize;
 import com.sew.backend.data.OrderState;
 
-@Entity(name = "EventInfo")
+@Entity(name = "Event")
 
 @Table(indexes = @Index(columnList = "VorschlagDate"))
 public class Event extends AbstractEntity
 {
+    @NotNull
+    @OneToOne(cascade = CascadeType.ALL)
+    private User ersteller;
 
+    @NotNull(message = "{bakery.due.date.required}")
+    private LocalDate dueDate;
+
+    @NotNull(message = "{bakery.due.time.required}")
+    private LocalTime dueTime;
+
+    private String name;
+
+    private String inhalt;
+
+    public Event(User createdBy) {
+        setErsteller(new User());
+        //addHistoryItem(createdBy, "Order placed");
+    }
+
+    public User getErsteller() {
+        return ersteller;
+    }
+
+    public void setErsteller(User ersteller) {
+        this.ersteller = ersteller;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalTime getDueTime() {
+        return dueTime;
+    }
+
+    public void setDueTime(LocalTime dueTime) {
+        this.dueTime = dueTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInhalt() {
+        return inhalt;
+    }
+
+    public void setInhalt(String inhalt) {
+        this.inhalt = inhalt;
+    }
 }
